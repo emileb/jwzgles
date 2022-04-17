67350dc389de1ccd4242b28903c3b36379310eba
Emile Belanger <emile.belanger@gmail.com>
2022-04-17 17:34:26 +0100

    Fix Make warnings

--- Android.mk
+++ Android.mk
@@ -1,9 +1,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-
 include $(CLEAR_VARS)
-
 
 LOCAL_MODULE    := jwzgles
 
@@ -11,10 +9,7 @@ LOCAL_CFLAGS :=   -O2  -DHAVE_ANDROID -DHAVE_JWZGLES
 
 LOCAL_SRC_FILES =  jwzgles.c
 
-LOCAL_LDLIBS :=  -ldl -llog -lGLESv1_CM -lEGL
-
 include $(BUILD_STATIC_LIBRARY)
-
 
 
 
